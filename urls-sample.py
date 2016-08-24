fbd017c4c29eebaeca3f83294e9d289453aca735
Adrien Barbaresi <barbaresi@bbaw.de>
2016-08-24 12:11:42 +0200

    require sorted order

--- urls-sample.py
+++ urls-sample.py
@@ -4,13 +4,17 @@
 ### (C) Adrien Barbaresi, 2016.
 
 # Purpose: filter a URL list and select a random subset of n URLs per domain name
+## takes a sorted URL list as input!
 
 
 import argparse
 from collections import defaultdict
+from functools import cmp_to_key
+import locale
 from random import sample
+import sys
 from urllib.parse import urlparse
-import validators
+import validators # https://github.com/kvesteri/validators
 
 
 parser = argparse.ArgumentParser()
@@ -18,16 +22,21 @@ parser.add_argument('-i', '--inputfile', dest='inputfile', help='input file', re
 parser.add_argument('-o', '--outputfile', dest='outputfile', help='output file', required=True)
 parser.add_argument('-s', '--size', dest='size', help='sample size', type=int, default=1000)
 parser.add_argument('-v', '--verbose', dest='verbose', help='verbose mode', action='store_true')
+parser.add_argument('--nowarnings', dest='nowarnings', help='disable sort warnings', action='store_true')
 args = parser.parse_args()
 
 
 urls = defaultdict(list)
 urlbuffer = list()
 lastseen = None
+locale.setlocale(locale.LC_ALL, "")
 
+
+# classify on the fly
 with open(args.inputfile, 'r', encoding='utf-8') as inputfh:
     with open(args.outputfile, 'w', encoding='utf-8') as outputfh:
         for line in inputfh:
+            # use standard parsing library
             parsed_url = urlparse(line.strip())
 
             # validate (https://github.com/gruns/furl ?)
@@ -38,22 +47,33 @@ with open(args.inputfile, 'r', encoding='utf-8') as inputfh:
             if validators.url(parsed_url.geturl()) is False:
                 continue
 
-            # initial
+            # initialize
             if lastseen is None:
                 lastseen = parsed_url.netloc
-            # key = r.scheme + '://' + r.netloc
-            value = parsed_url.geturl()
-            # continue collection
+
+            # dump URL
+            url = parsed_url.geturl()
+
+            ## continue collection
             if parsed_url.netloc == lastseen:
-                # urls[key].append(value)
-                urlbuffer.append(value)
-            # sample, drop, start new collection
+                urlbuffer.append(url)
+
+            ## sample, drop, fresh start
             else:
+                # test if sorted
+                if args.nowarnings is False:
+                    testlist = [lastseen, parsed_url.netloc]
+                    sortedtest = sorted(testlist, key=cmp_to_key(locale.strcoll))
+                    if sortedtest[0] != testlist[0]:
+                        sys.exit('input list not sorted, exiting.')
+
+                # write all the buffer
                 if len(urlbuffer) <= args.size:
                     for item in urlbuffer:
                         outputfh.write(item + '\n')
                     if args.verbose is True:
                         print (lastseen, '\t\turls:', len(urlbuffer))
+                # or sample URLs
                 else:
                     for item in sample(urlbuffer, args.size):
                         outputfh.write(item + '\n')
@@ -61,6 +81,6 @@ with open(args.inputfile, 'r', encoding='utf-8') as inputfh:
                         print (lastseen, '\t\turls:', len(urlbuffer), '\tprop.:', args.size/len(urlbuffer))
 
                 urlbuffer = []
-                urlbuffer.append(value)
+                urlbuffer.append(url)
 
             lastseen = parsed_url.netloc
