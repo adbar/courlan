e258885bed00ee79d22842bca5488951d5ca9011
Adrien Barbaresi <barbaresi@bbaw.de>
2020-09-01 16:30:41 +0200

    groups in CLI + output discarded URLs

--- courlan/cli.py
+++ courlan/cli.py
@@ -14,34 +14,40 @@ from .core import check_url, sample_urls
 def parse_args(args):
     """Define parser for command-line arguments"""
     argsparser = argparse.ArgumentParser()
-    argsparser.add_argument("-i", "--inputfile",
-                            help="""name of input file""",
+    group1 = argsparser.add_argument_group('I/O', 'Manage input and output')
+    group1.add_argument("-i", "--inputfile",
+                            help='name of input file (required)',
                             type=str, required=True)
-    argsparser.add_argument("-o", "--outputfile",
-                            help="""name of input file""",
+    group1.add_argument("-o", "--outputfile",
+                            help='name of output file (required)',
                             type=str, required=True)
-    argsparser.add_argument("-v", "--verbose",
+    group1.add_argument("-d", "--discardedfile",
+                            help='name of file to store discarded URLs (optional)',
+                            type=str)
+    group1.add_argument("-v", "--verbose",
                             help="increase output verbosity",
                             action="store_true")
-    argsparser.add_argument("--strict",
+    group2 = argsparser.add_argument_group('Filtering', 'Configure URL filters')
+    group2.add_argument("--strict",
                             help="perform more restrictive tests",
                             action="store_true")
-    argsparser.add_argument("-l", "--language",
+    group2.add_argument("-l", "--language",
                             help="use language filter",
                             action="store_true")
-    argsparser.add_argument("-r", "--redirects",
+    group2.add_argument("-r", "--redirects",
                             help="check redirects",
                             action="store_true")
-    argsparser.add_argument('--sample',
+    group3 = argsparser.add_argument_group('Sampling', 'Use sampling by host, configure sample size')
+    group3.add_argument('--sample',
                             help='use sampling',
                             action="store_true")
-    argsparser.add_argument('--samplesize',
+    group3.add_argument('--samplesize',
                             help='size of sample per domain',
                             type=int, default=1000)
-    argsparser.add_argument('--exclude-max',
+    group3.add_argument('--exclude-max',
                             help='exclude domains with more than n URLs',
                             type=int) # default=10000
-    argsparser.add_argument('--exclude-min',
+    group3.add_argument('--exclude-min',
                             help='exclude domains with less than n URLs',
                             type=int)
     return argsparser.parse_args()
@@ -52,12 +58,16 @@ def main():
     # arguments
     args = parse_args(sys.argv[1:])
     if args.sample is False:
-        with open(args.inputfile, 'r', encoding='utf-8', errors='ignore') as inputfh:
-            with open(args.outputfile, 'w', encoding='utf-8') as outputfh:
-                for line in inputfh:
-                    result = check_url(line, strict=args.strict, with_redirects=args.redirects, with_language=args.language)
-                    if result is not None:
-                        outputfh.write(result[0] + '\n')
+        with open(args.inputfile, 'r', encoding='utf-8', errors='ignore') as inputfh, \
+             open(args.outputfile, 'w', encoding='utf-8') as outputfh:
+            for line in inputfh:
+                result = check_url(line, strict=args.strict, with_redirects=args.redirects, with_language=args.language)
+                if result is not None:
+                    outputfh.write(result[0] + '\n')
+                # proceed with discarded URLs. to be rewritten
+                elif args.discardedfile is not None:
+                    with open(args.discardedfile, 'a', encoding='utf-8') as discardfh:
+                        discardfh.write(result[0] + '\n')
     else:
         urllist = list()
         with open(args.inputfile, 'r', encoding='utf-8', errors='ignore') as inputfh:
