1aa7692625f015d4b6fc4ea15f21412b067c8bf1
Adrien Barbaresi <barbaresi@bbaw.de>
2021-10-07 18:19:53 +0200

    code efficiency: use sets instead of tuples

--- courlan/settings.py
+++ courlan/settings.py
@@ -8,10 +8,10 @@ General settings for package execution.
 
 # https://www.alexa.com/topsites/countries/DE
 # https://www.alexa.com/topsites/countries/US
-BLACKLIST = ('akamai', 'aliexpress', 'amzn', 'amazon', 'amazonaws', 'baidu', 'bit', 'bongacams', 'chaturbate', 'cloudfront', 'delicious', 'digg', 'ebay', 'ebay-kleinanzeigen', 'facebook', 'feedburner', 'flickr', 'gettyimages', 'gmx', 'google', 'gravatar', 'http', 'imgur', 'immobilienscout24', 'instagr', 'instagram', 'last', 'linkedin', 'live', 'livejasmin', 'localhost', 'mail', 'netflix', 'ok', 'otto', 'paypal', 'pinterest', 'pornhub', 'postbank', 'qq', 'reddit', 'sina', 'sohu', 'soundcloud', 'taobao', 'telegram', 'tiktok', 'tmall', 'twitch', 'twitter', 'twitpic', 'txxx', 'vk', 'vkontakte', 'vimeo', 'web', 'weibo', 'whatsapp', 'xhamster', 'xvideos', 'yahoo', 'yandex', 'youtube', 'youtu', 'zoom')
+BLACKLIST = {'akamai', 'aliexpress', 'amzn', 'amazon', 'amazonaws', 'baidu', 'bit', 'bongacams', 'chaturbate', 'cloudfront', 'delicious', 'digg', 'ebay', 'ebay-kleinanzeigen', 'facebook', 'feedburner', 'flickr', 'gettyimages', 'gmx', 'google', 'gravatar', 'http', 'imgur', 'immobilienscout24', 'instagr', 'instagram', 'last', 'linkedin', 'live', 'livejasmin', 'localhost', 'mail', 'netflix', 'ok', 'otto', 'paypal', 'pinterest', 'pornhub', 'postbank', 'qq', 'reddit', 'sina', 'sohu', 'soundcloud', 'taobao', 'telegram', 'tiktok', 'tmall', 'twitch', 'twitter', 'twitpic', 'txxx', 'vk', 'vkontakte', 'vimeo', 'web', 'weibo', 'whatsapp', 'xhamster', 'xvideos', 'yahoo', 'yandex', 'youtube', 'youtu', 'zoom'}
 
-ALLOWED_PARAMS = ('aid', 'article_id', 'artnr', 'id', 'itemid', 'objectid', 'p', 'page', 'pagenum', 'page_id', 'pid', 'post', 'postid', 'product_id')
-CONTROL_PARAMS = ('lang', 'language')
-TARGET_LANG_DE = ('de', 'deutsch', 'ger', 'german')
-TARGET_LANG_EN = ('en', 'english', 'eng') # 'en_US', ''
+ALLOWED_PARAMS = {'aid', 'article_id', 'artnr', 'id', 'itemid', 'objectid', 'p', 'page', 'pagenum', 'page_id', 'pid', 'post', 'postid', 'product_id'}
+CONTROL_PARAMS = {'lang', 'language'}
+TARGET_LANG_DE = {'de', 'deutsch', 'ger', 'german'}
+TARGET_LANG_EN = {'en', 'english', 'eng'} # 'en_US', ''
 # accepted_lang = ('en')
