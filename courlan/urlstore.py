fd9bd6f5d0e4ed7856e45c6ef835260fbcac58a2
Adrien Barbaresi <barbaresi@bbaw.de>
2022-02-24 19:58:41 +0100

    urlstore consolidation: additional classes, functions and meta-info

--- courlan/urlstore.py
+++ courlan/urlstore.py
@@ -6,6 +6,7 @@ import bz2
 import logging
 
 from collections import defaultdict, deque
+from datetime import datetime
 from sys import getsizeof as size
 
 import _pickle as pickle  # import pickle
@@ -17,30 +18,35 @@ from .urlutils import get_host_and_path
 LOGGER = logging.getLogger(__name__)
 
 
-def recursive_size(myobject):
-    "Recursively approximatate the memory cost of a custom object."
-    # compressed bytestring
-    if isinstance(myobject, bytes):
-        return size(myobject)
-    # urlstore deque object
-    if isinstance(myobject, deque):
-        # return sum(size(()) + size(k) + size(v) for k, v in myobject) + size(myobject)
-        return sum(size(k) for k, _ in myobject) + (size(()) + size(False)) * len(myobject) + size(myobject)
-    raise ValueError('invalid object type: %s', type(myobject))
+class DomainEntry:
+    "Class to record host-related information and URL paths."
+    __slots__ = ('all_visited', 'timestamp', 'tuples')
+
+    def __init__(self):
+        self.all_visited = False
+        self.tuples = deque()
+        self.timestamp = None
+
+
+class UrlPathTuple:
+    "Class storing information for URL paths relative to a domain/host."
+    __slots__ = ('urlpath', 'visited')
+
+    def __init__(self, urlpath, visited):
+        self.urlpath = urlpath
+        self.visited = visited
 
 
 class UrlStore:
     "Defines a class to store domain-classified URLs and perform checks against it."
-    __slots__ = ('compressed', 'language', 'strict', 'urldict', 'validation', 'visited')
-    #__slots__ = ('__dict__')
+    __slots__ = ('compressed', 'language', 'strict', 'urldict', 'validation')
 
-    def __init__(self, compressed=False, strict=False, language=None, validation=True, visited=False):
+    def __init__(self, compressed=False, language=None, strict=False, validation=True):
         self.compressed = compressed
         self.language = language
         self.strict = strict
         self.urldict = {}
         self.validation = validation
-        self.visited = visited
 
     def _filter_url(self, url):
         # TODO: validate URL / check_url()?
@@ -55,7 +61,7 @@ class UrlStore:
             return [u for u in urls if self._filter_url(domain + u) is True]
         return urls
 
-    def _buffer_urls(self, data):
+    def _buffer_urls(self, data, visited=False):
         inputdict = defaultdict(deque)
         for url in list(dict.fromkeys(data)):
             # filter
@@ -64,31 +70,42 @@ class UrlStore:
             # segment URL and add to domain dictionary
             try:
                 hostinfo, urlpath = get_host_and_path(url)
-                inputdict[hostinfo].append((urlpath, self.visited))
+                inputdict[hostinfo].append(UrlPathTuple(urlpath, visited))
             except ValueError:
                 LOGGER.warning('Could not parse URL, discarding: %s', url)
         return inputdict
 
     def _load_urls(self, domain):
-        value = self.urldict.get(domain)
-        if value is not None:
+        if domain in self.urldict:
+            value = self.urldict[domain].tuples
             if isinstance(value, bytes):
                 return pickle.loads(bz2.decompress(value))
             return value
         return deque()
 
-    def _store_urls(self, domain, urls):
+    def _store_urls(self, domain, urls, set_timestamp=False):
+        # init
+        if domain not in self.urldict:
+            self.urldict[domain] = DomainEntry()
+        # compression
         if self.compressed is True:
             pickled = pickle.dumps(urls, protocol=4)
             new_value = bz2.compress(pickled)
             # be sure to make gains through compression
-            if recursive_size(new_value) < len(pickled):  # recursive_size(urls)
-            # and len(new_value) < len(pickled) / 6
-                self.urldict[domain] = new_value
+            if size(new_value) < size(pickled):
+                self.urldict[domain].tuples = new_value
             else:
-                self.urldict[domain] = urls
+                self.urldict[domain].tuples = urls
         else:
-            self.urldict[domain] = urls
+            self.urldict[domain].tuples = urls
+        # adjust all_visited status
+        if any(u.visited is False for u in urls):
+            self.urldict[domain].all_visited = False
+        else:
+            self.urldict[domain].all_visited = True
+        # timestamp/backoff value
+        if set_timestamp is True:
+            self.urldict[domain].timestamp = datetime.now()
 
     def _search_urls(self, urls, switch=None):
         # init
@@ -97,12 +114,18 @@ class UrlStore:
         # iterate
         for url in sorted(remaining_urls):
             hostinfo, urlpath = get_host_and_path(url)
+            # already seen
+            if hostinfo in self.urldict and self.urldict[hostinfo].all_visited is True:
+                del remaining_urls[url]
+                continue
+            # examine domain
             if hostinfo != last_domain:
                 last_domain = hostinfo
                 if switch == 1:
-                    known_paths = {u[0] for u in self._load_urls(hostinfo)}
+                    known_paths = {u.urlpath for u in self._load_urls(hostinfo)}
                 elif switch == 2:
-                    known_paths = {u[0]: u[1] for u in self._load_urls(hostinfo)}
+                    known_paths = {u.urlpath: u.visited for u in self._load_urls(hostinfo)}
+            # process result
             if not known_paths:
                 continue
             if switch == 1 and urlpath in known_paths:
@@ -112,11 +135,11 @@ class UrlStore:
         # preserve input order
         return list(remaining_urls)
 
-    def add_data(self, data):
-        for key, value in self._buffer_urls(data).items():
+    def add_data(self, data, visited=False):
+        for key, value in self._buffer_urls(data, visited).items():
             self._store_urls(key, value)
 
-    def extend_urls(self, domain, leftseries=[], rightseries=[]):
+    def extend_urls(self, domain, leftseries=[], rightseries=[], visited=False):
         # filter
         leftseries = self._filter_urlpaths(domain, leftseries)
         rightseries = self._filter_urlpaths(domain, rightseries)
@@ -124,34 +147,35 @@ class UrlStore:
             return
         # load data
         current_deque = self._load_urls(domain)
-        in_store = {u[0] for u in current_deque}
+        in_store = {u.urlpath for u in current_deque}
         # process and store
-        current_deque.extendleft([(u, self.visited) for u in leftseries if not u in in_store])
-        current_deque.extend([(u, self.visited) for u in rightseries if not u in in_store])
+        current_deque.extendleft([UrlPathTuple(u, visited) for u in leftseries if not u in in_store])
+        current_deque.extend([UrlPathTuple(u, visited) for u in rightseries if not u in in_store])
         self._store_urls(domain, current_deque)
 
     def get_url(self, domain):
-        candidate = None
+        # domain is full
+        if self.urldict[domain].all_visited is True:
+            return None
         url_tuples = self._load_urls(domain)
         i = 0
         # get first non-seen url
-        for urlpath, visited in url_tuples:
-            if visited is False:
-                candidate = urlpath
+        for url in url_tuples:
+            if url.visited is False:
                 # set visited to True
-                url_tuples[i] = (urlpath, True)
+                url.visited = True
                 break
             i += 1
         # store info
-        self._store_urls(domain, url_tuples)
-        return candidate
+        self._store_urls(domain, url_tuples, set_timestamp=True)
+        return domain + url.urlpath
 
     def is_known(self, url):
         hostinfo, urlpath = get_host_and_path(url)
         values = self._load_urls(hostinfo)
         if not values:
             return False
-        return urlpath in {u[0] for u in values}
+        return urlpath in {u.urlpath for u in values}
 
     def find_unknown_urls(self, urls):
         return self._search_urls(urls, switch=1)
@@ -161,11 +185,26 @@ class UrlStore:
         values = self._load_urls(hostinfo)
         if not values:
             return False
-        known_urlpaths = {u[0]: u[1] for u in values}
-        if urlpath not in known_urlpaths:
-            return False
-        return known_urlpaths[urlpath]
+        known_urlpaths = {u.urlpath: u.visited for u in values}
+        return known_urlpaths.get(urlpath) or False
 
     def find_unvisited_urls(self, urls):
         return self._search_urls(urls, switch=2)
+
+    def get_download_urls(self, limit=10):
+        now = datetime.now()
+        targets = []
+        potential = [d for d in self.urldict if self.urldict[d].all_visited is False]
+        if not potential:
+            return None
+        for domain in potential:
+            timestamp = self.urldict[domain].timestamp
+            if timestamp is None or (now - timestamp).total_seconds() > limit:
+                targets.append(domain)
+        # get corresponding URLs and filter out None values
+        return list(filter(None, [self.get_url(domain) for domain in targets]))
+
+    def dump_urls(self):
+        for domain in self.urldict:
+            print('\n'.join([domain + u.urlpath for u in self._load_urls(domain)]))
 
--- tests/urlstore_tests.py
+++ tests/urlstore_tests.py
@@ -6,6 +6,7 @@ import pickle
 import uuid
 
 from collections import deque
+from datetime import datetime
 
 from courlan import UrlStore
 
@@ -17,11 +18,15 @@ def test_urlstore():
     my_urls = UrlStore(validation=False)
     candidates = ['123', 'http://', 'ftp://example.org', 'http://example.org/']
     my_urls.add_data(candidates)
-    assert len(my_urls.urldict) == 1 and my_urls.urldict['http://example.org'] == deque([('/', False)])
+    assert len(my_urls.urldict) == 1
+    firstelem = my_urls.urldict['http://example.org'].tuples[0]
+    assert firstelem.urlpath == '/' and firstelem.visited is False
     # sanity checks
     my_urls = UrlStore(validation=True)
     my_urls.add_data(candidates)
-    assert len(my_urls.urldict) == 1 and my_urls.urldict['http://example.org'] == deque([('/', False)])
+    assert len(my_urls.urldict) == 1
+    firstelem = my_urls.urldict['http://example.org'].tuples[0]
+    assert firstelem.urlpath == '/' and firstelem.visited is False
     # filters
     my_urls = UrlStore(language='en', strict=True)
     candidates = ['https://de.wikipedia.org/', 'https://www.sitemaps.org/en_GB/protocol.html', 'http://example.com/de/test.html']
@@ -51,7 +56,10 @@ def test_urlstore():
     my_urls.add_data(urls)
     assert sum([len(my_urls._load_urls(k)) for k, _ in my_urls.urldict.items()]) == len(urls)
     if my_urls.compressed is False:
-        assert sum([len(v) for _, v in my_urls.urldict.items()]) == len(urls)
+        assert sum([len(v.tuples) for _, v in my_urls.urldict.items()]) == len(urls)
+    my_urls.add_data(['https://visited.com/visited'], visited=True)
+    assert my_urls.urldict['https://visited.com'].tuples[0].visited is True
+    assert my_urls.urldict['https://visited.com'].all_visited is True
 
     # test extension
     extension_urls = ['/1/' + str(a) for a in range(10)]
@@ -62,15 +70,23 @@ def test_urlstore():
     my_urls.extend_urls(example_domain, rightseries=extension_urls)
     url_tuples = my_urls._load_urls(example_domain)
     assert len(url_tuples) == len(example_urls) + 11
-    assert url_tuples[-1][0] == '/1/10'
+    assert url_tuples[-1].urlpath == '/1/10'
 
     # get_url
-    urlpath1 = my_urls.get_url(example_domain)
-    urlpath2 = my_urls.get_url(example_domain)
-    assert urlpath1 != urlpath2
+    assert my_urls.urldict[example_domain].timestamp is None
+    url1 = my_urls.get_url(example_domain)
+    url2 = my_urls.get_url(example_domain)
+    assert url1 != url2 and url1 == 'https://www.example.org/1/9'
     url_tuples = my_urls._load_urls(example_domain)
     # positions
-    assert url_tuples[0][0] == urlpath1 and url_tuples[1][0] == urlpath2
+    assert url1.endswith(url_tuples[0].urlpath)  and url2.endswith(url_tuples[1].urlpath)
+    # timestamp
+    assert my_urls.urldict[example_domain].timestamp is not None
+    # nothing left
+    assert my_urls.urldict[example_domain].all_visited is False
+    my_urls.add_data(['http://tovisit.com/page'])
+    assert my_urls.get_url('http://tovisit.com') == 'http://tovisit.com/page'
+    assert my_urls.urldict['http://tovisit.com'].all_visited is True
 
     # known or not
     assert my_urls.is_known('https://www.other.org/1') is False
@@ -78,11 +94,31 @@ def test_urlstore():
     candidates = ['https://test.org/category/this', 'https://www.example.org/1', 'https://otherdomain.org/']
     assert my_urls.find_unknown_urls(candidates) == ['https://test.org/category/this', 'https://otherdomain.org/']
     # visited or not
-    assert url_tuples[0][1] is True and url_tuples[1][1] is True and url_tuples[2][1] is False
+    assert url_tuples[0].visited is True and url_tuples[1].visited is True and url_tuples[2].visited is False
     assert my_urls.has_been_visited('https://www.other.org/1') is False
-    assert my_urls.has_been_visited(example_domain + urlpath1) is True
+    assert my_urls.has_been_visited(url1) is True
     assert my_urls.has_been_visited(example_domain + '/this') is False
     assert my_urls.has_been_visited(example_domain + '/999') is False
-    candidates = [example_domain + urlpath1, example_domain + '/this', example_domain + '/999']
+    candidates = [url1, example_domain + '/this', example_domain + '/999']
     assert my_urls.find_unvisited_urls(candidates) == [example_domain + '/this', example_domain + '/999']
+
+    # get download URLs
+    downloadable_urls = my_urls.get_download_urls(limit=0)
+    assert len(downloadable_urls) == 2 and downloadable_urls[0] == 'https://www.example.org/1/7'
+    assert (datetime.now() - my_urls.urldict['https://www.example.org'].timestamp).total_seconds() < 0.1
+    downloadable_urls = my_urls.get_download_urls()  # limit=10
+    assert len(downloadable_urls) == 0
+    other_one = UrlStore()
+    downloadable_urls = other_one.get_download_urls()
+    assert downloadable_urls is None
+
+
+
+def test_dbdump(capsys):
+    # database dump
+    other_one = UrlStore()
+    other_one.add_data(['http://print.org/print'])
+    other_one.dump_urls()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == 'http://print.org/print'
 
