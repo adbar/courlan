f426badc52caebd5da8b664131c2f82c67d08b12
Adrien Barbaresi <adbar@users.noreply.github.com>
2022-03-14 19:10:12 +0100

    urlutils.py: undo f-strings (2)

--- courlan/urlutils.py
+++ courlan/urlutils.py
@@ -58,7 +58,7 @@ def get_hostinfo(url):
 def fix_relative_urls(baseurl, url):
     'Prepend protocol and host information to relative links.'
     if url.startswith('//'):
-        return f'https:{url}' if baseurl.startswith('https') else f'http:{url}'
+        return 'https:' + url if baseurl.startswith('https') else 'http:' + url
     if url.startswith('/'):
         # imperfect path handling
         return baseurl + url
