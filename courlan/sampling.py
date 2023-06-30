c4620844e38ad98a217cc2d7356af296227841a2
Adrien Barbaresi <barbaresi@bbaw.de>
2023-06-30 12:12:40 +0200

    CLI sampling: consume input in chunks

--- courlan/sampling.py
+++ courlan/sampling.py
@@ -14,28 +14,14 @@ from .urlstore import UrlStore
 LOGGER = logging.getLogger(__name__)
 
 
-def sample_urls(
-    input_urls: List[str],
+def _make_sample(
+    urlstore: UrlStore,
     samplesize: int,
-    exclude_min: Optional[int] = None,
-    exclude_max: Optional[int] = None,
-    strict: bool = False,
-    verbose: bool = False,
+    exclude_min: Optional[int],
+    exclude_max: Optional[int],
 ) -> List[str]:
-    """Sample a list of URLs by domain name, optionally using constraints on their number"""
-    # logging
-    if verbose:
-        LOGGER.setLevel(logging.DEBUG)
-    else:
-        LOGGER.setLevel(logging.ERROR)
-    # store
+    "Iterate through the hosts in store and draw samples."
     output_urls = []
-    use_compression = len(input_urls) > 10**6
-    urlstore = UrlStore(
-        compressed=use_compression, language=None, strict=strict, verbose=verbose
-    )
-    urlstore.add_urls(sorted(input_urls))
-    # iterate
     for domain in urlstore.urldict:  # key=cmp_to_key(locale.strcoll)
         urlpaths = [
             p.urlpath
@@ -64,5 +50,25 @@ def sample_urls(
             len(mysample),
             len(mysample) / len(urlpaths),
         )
-    # return gathered URLs
     return output_urls
+
+
+def sample_urls(
+    input_urls: List[str],
+    samplesize: int,
+    exclude_min: Optional[int] = None,
+    exclude_max: Optional[int] = None,
+    strict: bool = False,
+    verbose: bool = False,
+) -> List[str]:
+    """Sample a list of URLs by domain name, optionally using constraints on their number"""
+    # logging
+    if verbose:
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        LOGGER.setLevel(logging.ERROR)
+    # store
+    urlstore = UrlStore(compressed=True, language=None, strict=strict, verbose=verbose)
+    urlstore.add_urls(input_urls)
+    # return gathered URLs
+    return _make_sample(urlstore, samplesize, exclude_min, exclude_max)
