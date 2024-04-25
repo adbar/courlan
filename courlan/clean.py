6ac97e402a6c93ff89c4843327593706ef07d1e0
Adrien Barbaresi <barbaresi@bbaw.de>
2024-04-25 18:40:39 +0200

    perf test

--- courlan/clean.py
+++ courlan/clean.py
@@ -60,7 +60,7 @@ def scrub_url(url: str) -> str:
     # strip space in input string
     url = "".join(url.split()).strip(
         "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
-        "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f \r\n"
+        "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
     )
 
     # <![CDATA[http://www.urbanlife.de/item/260-bmw-i8-hybrid-revolution-unter-den-sportwagen.html]]>
@@ -112,10 +112,10 @@ def clean_query(
     querystring: str, strict: bool = False, language: Optional[str] = None
 ) -> str:
     "Strip unwanted query elements"
-    qdict = parse_qs(querystring)
-    if not qdict:
+    if not querystring:
         return ""
 
+    qdict = parse_qs(querystring)
     newqdict = {}
 
     for qelem in sorted(qdict):
