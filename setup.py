59d3b014ad023f1d7488f1cddaea53885617ef7e
Adrien Barbaresi <adbar@users.noreply.github.com>
2024-04-30 13:11:27 +0200

    maintenance: convert readme to markdown (#97)
    * prepare
    
    * add README.md
    
    * fixes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_version(package):
 
 def get_long_description():
     "Return the README"
-    with open("README.rst", "r", encoding="utf-8") as filehandle:
+    with open("README.md", "r", encoding="utf-8") as filehandle:
         long_description = filehandle.read()
     # long_description += "\n\n"
     # with open("CHANGELOG.md", encoding="utf8") as f:
@@ -55,6 +55,7 @@ setup(
     version=get_version("courlan"),
     description="Clean, filter and sample URLs to optimize data collection – includes spam, content type and language filters.",
     long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Development Status :: 4 - Beta",
